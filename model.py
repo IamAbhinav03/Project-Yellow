c98598b415331dea7f4ef52435f317edbe6fd313
Abhinav M Hari <66298497+IamAbhinav03@users.noreply.github.com>
2022-10-12 00:20:36 +0530

    Update model.py
    Added test code for database. Current status: NOT WORKING.
    
    Explore the issues with imports

--- model.py
+++ model.py
@@ -2,12 +2,45 @@
 File to manage databases
 """
 
-from . import settings
+from settings import DEBUG
 import sqlite3
+import datetime
 
-if settings.DEBUG:
+if DEBUG:
 	"""
 	If DEBUG is true, create a database in memory with a few random entries
 	else, create/connect to the existing database
 	"""
-	pass
+	conn = sqlite3.connect(":memory")
+	print("[OK] Created test database in memory")
+	# In order to execute SQL statements and fetch results from SQL queries, 
+	# we will need to use a database cursor. Call con.cursor() to create the Cursor:
+	cursor = conn.cursor()
+
+	conn.execute("""CREATE TABLE coustomer
+		(ID INT PRIMARY KEY NOT NULL,
+		VEHICLE_NO TEXT NOT NULL,
+		PHONE_NO INT NOT NULL,
+		TYPE TEXT NOT NULL,
+		TEST_DATE DATE NOT NULL,
+		TEST_TIME TIMESTAMP NOT NULL,
+		EXPIRY DATE NOT NULL
+		""")
+
+	today = datetime.date.today()
+	now = datetime.datetime.now()
+	expiry = datetime.date.fromisoformat("2023-10-04")
+
+	cursor.execute("""INSERT INTO coustomer
+		(ID, VEHICLE_NO, PHONE_NO,
+		TYPE, TEST_DATE, TEST_TIME,
+		EXPIRY)
+		VALUES (?, ?, ?, ?, ?, ?, ?)""",
+		(1, "kl51k0000", 1234567890, 
+		"JEEP", today, now, expiry))
+
+	cursor.execute("""SELECT * FROM coustomer""")
+	data = cursor.fetchall()
+	print(data)
+
+
